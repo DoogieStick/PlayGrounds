f5abc91326e88f106003458a9cf47a8513b37ee9
Doogie <doogie.stickrd@gmail.com>
2018-02-27 22:37:57 -0300

    Parks Loading

--- src/app/playground/playground.component.ts
+++ src/app/playground/playground.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Playground } from './playground';
+import { PLAYGROUNDS_ON_LOAD } from './playground-data';
 
 declare var document : any;
+declare var console : any;
 
 @Component({
   selector: 'app-playground',
@@ -11,12 +13,17 @@ declare var document : any;
 export class PlaygroundComponent implements OnInit {
     
     home = 'Playgrounds';
+    private playsOnload = PLAYGROUNDS_ON_LOAD;
+    
+    playgrounds : Playground[] = this.playsOnload.splice(1,this.playsOnload.length);
     
     showAddPlaygroundForm(){
         console.info("it works");    
     }
     
-    playgrounds : Playground[] = [];
+    deletePlayground(){
+        console.info("it works");    
+    }
     
   constructor() { }
 
--- src/app/playground/playground.ts
+++ src/app/playground/playground.ts
@@ -1,11 +1,13 @@
 export interface Playground {
   
-    id: number;
-    name: string;
-    description: string;
-    address: string;
-    locality: string;
-    state: string;
-    country: string;  
+    [index: number]: {
+        id: number;
+        name: string;
+        description: string;
+        address: string;
+        locality: string;
+        state: string;
+        country: string;  
+    }    
     
 }
