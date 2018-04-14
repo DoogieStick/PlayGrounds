f2c14b1935d19482e36c745b73459ab00d6b55ce
gustavo <gustavopctomas@gmail.com>
2018-04-14 18:01:35 -0300

    equipamentos

--- src/app/equipment/equipment.service.ts
+++ src/app/equipment/equipment.service.ts
@@ -11,10 +11,26 @@ export class EquipmentService {
       return this.equipmentsOnload
     }    
     
+	
     deleteProduct(id) {
-      this.equipmentsOnload.splice(this.equipmentsOnload.indexOf(id), 1);
+      this.equipmentsOnload.forEach((item, index) => {
+            if(item.id === id){
+                this.equipmentsOnload.splice(index , 1);    
+            }
+      });      
     }
 	
+    getEquipmentToEdit(id){
+        for (let entry of this.equipmentsOnload) {
+            if(id == entry.id){
+                return entry;
+            }
+        }
+    }
+    
+    editEquipment(equipment){
+        this.equipmentsOnload[equipment.id - 1] = equipment;
+    }	
 
   constructor() { }
 
