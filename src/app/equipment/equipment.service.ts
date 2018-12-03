babade6355a5d40baaa941286c162bfeccc2f90f
gustavopctomas <31632597+gustavopctomas@users.noreply.github.com>
2018-12-03 00:39:40 -0200

    Add files via upload

--- src/app/equipment/equipment.service.ts
+++ src/app/equipment/equipment.service.ts
@@ -12,26 +12,43 @@ export class EquipmentService {
     constructor(private http: Http, private router : Router) {}
 
     public equipmentsOnload = EQUIPMENTS_ON_LOAD;
-
+	public equipmentsLength = 0;
     private values: Array<Equipment> = [];
     private anyErrors: boolean;
     private finished: boolean;
     public equipments: any = [];
-    public url = 'https://2l1kixhiw8.execute-api.sa-east-1.amazonaws.com/Playgrounds/playgrounds';        
+    public url = 'https://2l1kixhiw8.execute-api.sa-east-1.amazonaws.com/Playgrounds/playgrounds'; 
+    private cache = new Map();	
 
+	
     getEquipmentsFromData(): Promise<any> {
+  	   
         document.getElementById('dim').style.display = "block";
         return new Promise((resolve, reject) => {
-
-        let options = new RequestOptions({ headers: new Headers({'x-api-key': 'qmbGnJ4yQONHUgZT2ZJn1RW4x3jshSvas24L7YKg',
-        'tableName':'Equipments','quantity': 500}) });
-		return this.http.get(this.url, options)
-			   .map(response => response.json()).subscribe(result => {
-                this.equipmentsOnload = result.Items,
+        
+        if(this.cache.get("equipments")){
+            this.equipmentsOnload = this.cache.get("equipments").value;
+			console.log(this.cache.get("equipments"));
+            this.equipmentsLength = Math.max.apply(Math,this.equipmentsOnload.map(function(play) { return play.id; }));
+			console.log(this.equipmentsLength);
+            document.getElementById('dim').style.display = "none",
+            resolve(this.equipmentsOnload);
+        }else{
+			console.log("aqui2");		
+            let options = new RequestOptions({ headers: new Headers({'x-api-key': 'qmbGnJ4yQONHUgZT2ZJn1RW4x3jshSvas24L7YKg',
+            'tableName':'Equipments','quantity': 500}) });
+            
+             this.http.get(this.url, options)	 
+               .map(response => response.json()).subscribe(result => {
+                this.equipmentsOnload = result.Items.sort(function (a, b) {return a.id - b.id;}),
+                this.cache.set("equipments", { value: this.equipmentsOnload, expiry: Date.now() + 300000 }),   
+                this.equipmentsLength = Math.max.apply(Math,this.equipmentsOnload.map(function(play) { return play.id; })),
                 document.getElementById('dim').style.display = "none",
-                resolve(result);
+                resolve(this.equipmentsOnload);
             });
-       });
+        }    
+        
+       });	   
 
     }    
     
