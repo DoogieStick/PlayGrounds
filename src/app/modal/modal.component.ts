49ee47ed3325d5c5734637d8c0fc5549de6f0470
gustavopctomas <31632597+gustavopctomas@users.noreply.github.com>
2018-12-08 20:30:14 -0200

    modal - alert

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -14,12 +14,35 @@ export class ModalComponent {
     document.getElementById("modal").style.display = "block";
     document.getElementById("title").innerHTML = title;
     document.getElementById("desc").innerHTML = desc;
+	document.getElementById("cancel").style.display = "inline";
+	document.getElementById("alert").style.display = "none";
   }
+  
+  openModalAlert(title , desc){
+    document.getElementById("backdrop").style.display = "block";
+    document.getElementById("modal").style.display = "block";
+    document.getElementById("title").innerHTML = title;
+    document.getElementById("desc").innerHTML = desc;
+	document.getElementById("cancel").style.display = "none";
+	document.getElementById("alert").style.display = "inline";
+  }  
 
   onCloseHandled(){
     document.getElementById("backdrop").style.display = "none";
     document.getElementById("modal").style.display = "none";
     document.getElementById("title").innerHTML = "";
     document.getElementById("desc").innerHTML = "";
+	document.getElementById("alert").style.display = "none";
   }
+  
+  onCloseAlertHandled():boolean{
+    document.getElementById("backdrop").style.display = "none";
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("title").innerHTML = "";
+    document.getElementById("desc").innerHTML = "";
+	document.getElementById("cancel").style.display = "none";
+	document.getElementById("alert").style.display = "none";
+	return true;
+  }  
+  
 }
